ce3af2c569e3123e8830c7f55ce8fdaf71be84b8
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-08-19 10:48:09 -0400

    Laying groundwork for video capture

--- free-throw-pro-ios/SessionDetailViewController.swift
+++ free-throw-pro-ios/SessionDetailViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class SessionDetailViewController: UIViewController, UITextFieldDelegate {
+class SessionDetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet var numberField: UITextField!
     @IBOutlet var dateRecorded: UILabel!
     @IBOutlet var timeRecorded: UILabel!
     @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     @IBAction func backgroundTapped(_ sender: Any) {
         view.endEditing(true)
@@ -51,5 +65,12 @@ class SessionDetailViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
     }
 }
