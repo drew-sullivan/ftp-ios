e6924da6362f7d5fe8f3224fe533bccf6608db5b
Drew Sullivan <dsullivan@dev-030416.local>
2018-08-20 16:42:58 -0400

    Wiring things up. Added microphone permissions

--- free-throw-pro-ios/RecordVideoViewController.swift
+++ free-throw-pro-ios/RecordVideoViewController.swift
@@ -15,10 +15,31 @@ class RecordVideoViewController: UIViewController {
         MediaHelper.startMediaBrowser(delegate: self, sourceType: .camera)
     }
     
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension RecordVideoViewController: UIImagePickerControllerDelegate {
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard
+            let mediaType = info[UIImagePickerControllerMediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let url = info[UIImagePickerControllerMediaURL] as? URL,
+            UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+            else {
+                return
+        }
+        
+        UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
 }
 
 extension RecordVideoViewController: UINavigationControllerDelegate {
